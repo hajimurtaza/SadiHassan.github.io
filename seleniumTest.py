7c1ac4966d6d11441c727748b26d50a21d9d53a8
Sadi Hassan <sadi.research@gmail.com>
2019-09-13 14:49:45 -0300

    leetcode scraping++

--- seleniumTest.py
+++ seleniumTest.py
@@ -1,12 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+import sys
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+driver.get("https://leetcode.com/problemset/all/")
+driver.implicitly_wait(20)
+
+problem_link = driver.find_element_by_xpath('//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[1]/tr[1]/td[3]/div/a')
+
+
+problem_name = driver.find_element_by_xpath('//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[1]/tr[1]/td[3]')
+problem_id = driver.find_element_by_xpath('//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[1]/tr[1]/td[2]')
+
+problem_link2 = driver.find_element_by_xpath('//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[1]/tr[1]/td[3]/div')
+print('problem_id: ', problem_id.text)
+print('problem_name: ', problem_name.text)
+print('problem_link: ', problem_link.get_attribute('href'))
+problem_link.click()
+input("Press any key to exit...")
+driver.close()	
+
+
+'''
+#//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[1]/tr[1]/td[3]/div/a
+try:
+	problem_name = WebDriverWait(driver, 100).until(
+        #EC.presence_of_element_located((By.ID, "myDynamicElement"))
+		EC.find_element_by_xpath('//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[1]/tr[1]/td[3]')
+		#print(problem_name)
+    )
+finally:
+	problem_link.click()
+	print(problem_name)
+	input("Press any key to exit...")
+	driver.close()	
+'''
+#input("Press any key to exit...")
+
+
